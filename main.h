553c9c6e7d7574e2b03d965bb3edb4207182a1ce
sessofmars <99624302+sessofmars@users.noreply.github.com>
2022-04-20 01:27:59 +0100

    never give up

--- main.h
+++ main.h
@@ -30,13 +30,13 @@ char c;
 int (*f)(va_list ap, flags_t *f);
 } ph;
 
-/* print_nums */
+/* printf_int */
 int print_int(va_list l, flags_t *f);
 void print_number(int n);
 int print_unsigned(va_list l, flags_t *f);
 int count_digit(int i);
 
-/* print_bases */
+/* printf_hex */
 int print_hex(va_list l, flags_t *f);
 int print_hex_big(va_list l, flags_t *f);
 int print_binary(va_list l, flags_t *f);
@@ -45,7 +45,7 @@ int print_octal(va_list l, flags_t *f);
 /* converter */
 char *convert(unsigned long int num, int base, int lowercase);
 
-/* _printf */
+/* 0-printf */
 int _printf(const char *format, ...);
 
 /* get_print */
@@ -54,15 +54,15 @@ int (*get_print(char s))(va_list, flags_t *);
 /* get_flag */
 int get_flag(char s, flags_t *f);
 
-/* print_alpha */
+/* printf_loops */
 int print_string(va_list l, flags_t *f);
 int print_char(va_list l, flags_t *f);
 
-/* write_funcs */
+/* printf_putchar */
 int _putchar(char c);
 int _puts(char *str);
 
-/* print_custom */
+/* printf_non_printable_characters */
 int print_rot13(va_list l, flags_t *f);
 int print_rev(va_list l, flags_t *f);
 int print_bigS(va_list l, flags_t *f);
